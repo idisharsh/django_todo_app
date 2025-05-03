e45146add5ca08ef7c8b5663c0553ff2b30055a4
idisharsh <idisharsh@gmail.com>
2025-05-03 11:45:24 +0530

    ToDo app 2nd commit- bug fixation

--- to_do_app/to_do_app/settings.py
+++ to_do_app/to_do_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-hhje-nc8f%5j+m-pi(au(b55d^-$)td&wxpwq(!85d9gexc8d4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.8']
 
 
 # Application definition
